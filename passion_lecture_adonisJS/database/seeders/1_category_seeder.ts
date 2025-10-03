6a21ddc874ec2da29a5ab264706124fc762e463c
LucaLeFich <pq48wkn@eduvaud.ch>
2025-10-03 13:50:02 +0200

    category seeder

--- passion_lecture_adonisJS/database/seeders/1_category_seeder.ts
+++ passion_lecture_adonisJS/database/seeders/1_category_seeder.ts
@@ -1,7 +1,15 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Category from '#models/category'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
+
+    await Category.createMany([
+      { label: 'Action'},
+      { label: 'Thriller'},
+      { label: 'Romance'},
+      { label: 'Horreur'},
+      { label: 'Science-Fiction'}
+    ])
   }
 }
