383352545166b4af838d9d31c19dcc4b8a78b9e4
Nelson Almeida <pf14wzd@eduvaud.ch>
2025-10-03 12:03:37 +0200

    feat(writer) controller queries

--- passion_lecture_adonisJS/app/controllers/writers_controller.ts
+++ passion_lecture_adonisJS/app/controllers/writers_controller.ts
@@ -1,38 +1,50 @@
+import Writer from '#models/writer'
+import { writerValidator } from '#validators/writer'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class WritersController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({ response }: HttpContext) {
+    const writers = await Writer.query().orderBy('name').orderBy('firstname')
+    return response.ok(writers)
+  }
 
   /**
-   * Display form to create a new record
+   * Add a writer
    */
-  async create({}: HttpContext) {}
-
-  /**
-   * Handle form submission for the create action
-   */
-  async store({ request }: HttpContext) {}
-
-  /**
-   * Show individual record
-   */
-  async show({ params }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    const { name, firstname } = await request.validateUsing(writerValidator)
+    const writer = await Writer.create({ name, firstname })
+    return response.created(writer) // 201
+  }
 
   /**
    * Edit individual record
    */
-  async edit({ params }: HttpContext) {}
+  async show({ params, response }: HttpContext) {
+    const writer = await Writer.findOrFail(params.id)
+    return response.ok(writer) // 200
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    const { name, firstname } = await request.validateUsing(writerValidator)
+    const writer = await Writer.findOrFail(params.id)
+    writer.merge({ name, firstname })
+    await writer.save()
+    return response.ok(writer) // 200
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    const writer = await Writer.findOrFail(params.id)
+    await writer.delete()
+    return response.noContent() // 204 (aucun contenu à retourner)
+  }
 }
