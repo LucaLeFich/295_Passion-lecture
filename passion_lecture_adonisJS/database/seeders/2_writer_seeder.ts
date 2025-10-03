5d86e889377babf74708509e0201986abff90a28
Nelson Almeida <pf14wzd@eduvaud.ch>
2025-10-03 10:47:19 +0200

    feat(writer) seeder creates 10 writers

--- passion_lecture_adonisJS/database/seeders/2_writer_seeder.ts
+++ passion_lecture_adonisJS/database/seeders/2_writer_seeder.ts
@@ -1,7 +1,19 @@
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Writer from '#models/writer'
 
-export default class extends BaseSeeder {
-  async run() {
-    // Write your database queries inside the run method
+export default class WriterSeeder extends BaseSeeder {
+  public async run() {
+    await Writer.createMany([
+      { firstname: 'George', name: 'Orwell' },
+      { firstname: 'Jane', name: 'Austen' },
+      { firstname: 'Haruki', name: 'Murakami' },
+      { firstname: 'J.K.', name: 'Rowling' },
+      { firstname: 'Agatha', name: 'Christie' },
+      { firstname: 'Leo', name: 'Tolstoy' },
+      { firstname: 'Isabel', name: 'Allende' },
+      { firstname: 'Victor', name: 'Hugo' },
+      { firstname: 'Mary', name: 'Shelley' },
+      { firstname: 'Stephen', name: 'King' },
+    ])
   }
 }
