27bd45bd1380dff3f50bb252bd5e065b46def5c2
LucaLeFich <pq48wkn@eduvaud.ch>
2025-10-03 11:35:34 +0200

    Validator category

--- passion_lecture_adonisJS/app/validators/category.ts
+++ passion_lecture_adonisJS/app/validators/category.ts
@@ -1,1 +1,8 @@
 import vine from '@vinejs/vine'
+
+const categoriesValidator = vine.compile(
+    vine.object({
+        label: vine.string().minLength(2).maxLength(255),
+    })
+)
+export { categoriesValidator }
