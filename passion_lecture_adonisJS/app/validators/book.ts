db3a79300e15018b1885c8eff219d4d43dad64cd
Veronika-blub <pu74vys@eduvaud.ch>
2025-10-03 12:00:30 +0200

    feat(validator): creation de bookValidator
    ajout de Validator

--- passion_lecture_adonisJS/app/validators/book.ts
+++ passion_lecture_adonisJS/app/validators/book.ts
@@ -1,1 +1,16 @@
 import vine from '@vinejs/vine'
+
+const bookValidator = vine.compile(
+    vine.object({
+        title: vine.string().minLength(3).maxLength(255),
+        numberOfPage: vine.number().min(1),
+        pdfLink: vine.string().url(),
+        abstract: vine.string().minLength(10),
+        editor: vine.string().minLength(2).maxLength(255),
+        editionYear: vine.number().max(new Date().getFullYear()),
+        imagePath: vine.string().url(),
+        idCategory: vine.number(),
+        idWriter: vine.number(),
+        idUser: vine.number(),
+    })
+)
