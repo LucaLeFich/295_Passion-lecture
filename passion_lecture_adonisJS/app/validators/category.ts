8ab08fac50bdb1ef22216af8023d448de50372d4
LucaLeFich <pq48wkn@eduvaud.ch>
2025-10-10 14:04:50 +0200

    Validator category notSameAs

--- passion_lecture_adonisJS/app/validators/category.ts
+++ passion_lecture_adonisJS/app/validators/category.ts
@@ -2,7 +2,7 @@ import vine from '@vinejs/vine'
 
 const categoriesValidator = vine.compile(
     vine.object({
-        label: vine.string().minLength(2).maxLength(255),
+        label: vine.string().minLength(2).maxLength(255).notSameAs("")
     })
 )
 export { categoriesValidator }
