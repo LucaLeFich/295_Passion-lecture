90e0265a8069f53255931c9c647a4f2456fb186c
Nelson Almeida <pf14wzd@eduvaud.ch>
2025-10-10 09:17:41 +0200

    Update validator user.ts
    not finished

--- passion_lecture_adonisJS/app/validators/user.ts
+++ passion_lecture_adonisJS/app/validators/user.ts
@@ -1,1 +1,12 @@
 import vine from '@vinejs/vine'
+
+const userValidator = vine.compile(
+  vine.object({
+    username: vine.string().minLength(2).maxLength(255),
+    hashPassword: vine.string().minLength(2).maxLength(255),
+    //  creationDate: vine.
+    //  isAdmin: vine.bool
+  })
+)
+
+export { userValidator }
