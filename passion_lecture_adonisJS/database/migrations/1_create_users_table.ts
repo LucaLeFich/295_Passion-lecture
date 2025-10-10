2b2c11ff5c636f175d2f4bf5023dd9e72a36d92a
Nelson Almeida <pf14wzd@eduvaud.ch>
2025-10-10 14:00:21 +0200

    Update migrations 1_create_users_table.ts

--- passion_lecture_adonisJS/database/migrations/1_create_users_table.ts
+++ passion_lecture_adonisJS/database/migrations/1_create_users_table.ts
@@ -5,13 +5,15 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
-      table.string('password').notNullable()
+      table.increments('id_user').primary() // correspond à idUser
 
-      table.timestamp('created_at').notNullable()
-      table.timestamp('updated_at').nullable()
+      table.string('username').nullable()
+      table.string('hash_password').notNullable() // correspond à hashPassword
+      table.boolean('is_admin').notNullable().defaultTo(false)
+      table.date('creation_date').notNullable()
+
+      table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).nullable()
     })
   }
 
