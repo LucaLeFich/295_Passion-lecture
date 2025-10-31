871d09127366b349026b85ff9c6241c2f7258ae8
Veronika-blub <pu74vys@eduvaud.ch>
2025-10-31 10:50:20 +0100

    feat(requêtes, validateur, seeder): bruno, validateur
    requêtes sur bruno pour book, category, comment, evaluate
    ajout de validator et seeder Evaluate

--- passion_lecture_adonisJS/app/validators/evaluate.ts
+++ passion_lecture_adonisJS/app/validators/evaluate.ts
@@ -1,1 +1,9 @@
 import vine from '@vinejs/vine'
+const evaluateValidator = vine.compile(
+    vine.object({
+        content: vine.number().min(1).max(5),
+        idUser: vine.number(),
+        idBook: vine.number(),
+    })
+)
+export { evaluateValidator }
--- passion_lecture_adonisJS/database/seeders/6_evaluate_seeder.ts
+++ passion_lecture_adonisJS/database/seeders/6_evaluate_seeder.ts
@@ -1,7 +1,19 @@
+import Evaluate from '#models/evaluate'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
+    await Evaluate.createMany([
+      {
+        note: 3,
+        userId: 1,
+        bookId: 1,
+      },
+      {
+        note: 4,
+        userId: 2,
+        bookId: 2,
+      },
+    ])
   }
 }
