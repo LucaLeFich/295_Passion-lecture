6eeaba86ac296446497e66effb180165b1a84515
LucaLeFich <pq48wkn@eduvaud.ch>
2025-10-31 10:07:29 +0100

    Feat(routes): routes
    Co-Authored-By: Nelson Almeida <195763919+NelsonAlmeida5@users.noreply.github.com>
    Co-Authored-By: Veronika-blub <195764010+Veronika-blub@users.noreply.github.com>

--- passion_lecture_adonisJS/start/routes.ts
+++ passion_lecture_adonisJS/start/routes.ts
@@ -13,21 +13,37 @@ import CategoriesController from '#controllers/categories_controller'
 import UsersController from '#controllers/users_controller'
 import AuthController from '#controllers/auth_controller'
 import { middleware } from './kernel.js'
-// Route test
-router.get('test', async () => {
-  return { message: 'API is working!' }
-})
+import EvaluatesController from '#controllers/evaluates_controller'
+import CommentsController from '#controllers/comments_controller'
 
-// Routes REST
-router.resource('writers', WritersController).apiOnly()
-router.resource('books', BooksController).apiOnly()
-router.resource('categories', CategoriesController).apiOnly()
-router.resource('users', UsersController).apiOnly()
 
 router
-    .group(() => {
-        router.post('register', [AuthController, 'register'])
-        router.post('login', [AuthController, 'login'])
-        router.post('logout', [AuthController, 'logout']).use(middleware.auth())
-    })
-    .prefix('user')
+  .group(() => {
+    router.resource('books', BooksController).apiOnly()
+    router.resource('writers', WritersController).apiOnly()
+    router.resource('categories', CategoriesController).apiOnly()
+    router.resource('users', UsersController).apiOnly()
+
+    // /books/:book_id/comments
+    router
+      .group(() => {
+        router.resource('comments', CommentsController).apiOnly()
+      })
+      .prefix('books/:book_id')
+
+    // /books/:book_id/evaluations
+    router
+      .group(() => {
+        router.resource('evaluations', EvaluatesController).apiOnly()
+      })
+      .prefix('books/:book_id')
+  })
+  .use(middleware.auth())
+
+router
+  .group(() => {
+    router.post('register', [AuthController, 'register'])
+    router.post('login', [AuthController, 'login'])
+    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+  })
+  .prefix('user')
