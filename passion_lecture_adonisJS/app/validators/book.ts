994e82de1de9368893a7576b60be4996d0e3c38d
Veronika-blub <pu74vys@eduvaud.ch>
2025-10-31 11:13:12 +0100

    fix(book) validator
    suppression de verification de url

--- passion_lecture_adonisJS/app/validators/book.ts
+++ passion_lecture_adonisJS/app/validators/book.ts
@@ -4,11 +4,11 @@ const bookValidator = vine.compile(
     vine.object({
         title: vine.string().minLength(3).maxLength(255),
         numberOfPage: vine.number().min(1),
-        pdfLink: vine.string().url(),
+        pdfLink: vine.string(),
         abstract: vine.string().minLength(10),
         editor: vine.string().minLength(2).maxLength(255),
         editionYear: vine.number().max(new Date().getFullYear()),
-        imagePath: vine.string().url(),
+        imagePath: vine.string(),
         idCategory: vine.number(),
         idWriter: vine.number(),
         idUser: vine.number(),
